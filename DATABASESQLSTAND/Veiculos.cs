726958a3b7391e5f0efae52abfcd0737123f0dee
Roberto Castro <115174164+RobertoCastro391@users.noreply.github.com>
2023-05-20 14:53:29 +0100

    initial

---
+++ DATABASESQLSTAND/Veiculos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATABASESQLSTAND
+{
+    internal class Veiculos
+    {
+        public string Matricula { get; set; }
+        public int id_Marca { get; set; }
+        public int id_stand { get; set; }
+        public String Modelo { get; set; }
+        public int id_Categoria { get; set; }
+        public int Ano { get; set; }
+        public int Cilindrada { get; set; }
+        public int id_Cor { get; set; }
+        public int Quilometros { get; set; }
+        public int id_Combustivel{ get; set; }
+        public int Preco_Anunciado { get; set; }
+        public String Observações { get; set; }
+    }
+}
---
+++ DATABASESQLSTAND/VeiculosDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATABASESQLSTAND
+{
+    internal class VeiculosDAO
+    {
+        List<Veiculos> veiculos = new List<Veiculos>();
+
+
+    }
+}
